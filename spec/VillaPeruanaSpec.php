3e03c7bcca2d5207fd09e425bbcea69e23e3b23c
Wilfred Lemus <wilfred.lemus@gmail.com>
2021-05-27 22:48:06 -0600

    [ADD] new product cafe and refactoring VillaPeruana

--- spec/VillaPeruanaSpec.php
+++ spec/VillaPeruanaSpec.php
@@ -245,63 +245,63 @@ describe('Villa Peruana', function () {
         });
 
 
-        // context ("Producto de Café", function () {
+        context ("Producto de Café", function () {
 
-        //     it ('actualiza Producto de Café antes de la fecha de venta', function () {
-        //         $item = GildedRose::of('Café Altocusco', 10, 10);
+            it ('actualiza Producto de Café antes de la fecha de venta', function () {
+                $item = VillaPeruana::of('Café Altocusco', 10, 10);
 
-        //         $item->tick();
+                $item->tick();
 
-        //         expect($item->quality)->toBe(8);
-        //         expect($item->sellIn)->toBe(9);
-        //     });
+                expect($item->quality)->toBe(8);
+                expect($item->sellIn)->toBe(9);
+            });
 
-        //     it ('actualiza Producto de Café con cualidad 0', function () {
-        //         $item = GildedRose::of('Café Altocusco', 0, 10);
+            it ('actualiza Producto de Café con cualidad 0', function () {
+                $item = VillaPeruana::of('Café Altocusco', 0, 10);
 
-        //         $item->tick();
+                $item->tick();
 
-        //         expect($item->quality)->toBe(0);
-        //         expect($item->sellIn)->toBe(9);
-        //     });
+                expect($item->quality)->toBe(0);
+                expect($item->sellIn)->toBe(9);
+            });
 
-        //     it ('actualiza Producto de Café en la fecha de venta', function () {
-        //         $item = GildedRose::of('Café Altocusco', 10, 0);
+            it ('actualiza Producto de Café en la fecha de venta', function () {
+                $item = VillaPeruana::of('Café Altocusco', 10, 0);
 
-        //         $item->tick();
+                $item->tick();
 
-        //         expect($item->quality)->toBe(6);
-        //         expect($item->sellIn)->toBe(-1);
-        //     });
+                expect($item->quality)->toBe(6);
+                expect($item->sellIn)->toBe(-1);
+            });
 
-        //     it ('actualiza Producto de Café en la fecha de venta con calidad 0', function () {
-        //         $item = GildedRose::of('Café Altocusco', 0, 0);
+            it ('actualiza Producto de Café en la fecha de venta con calidad 0', function () {
+                $item = VillaPeruana::of('Café Altocusco', 0, 0);
 
-        //         $item->tick();
+                $item->tick();
 
-        //         expect($item->quality)->toBe(0);
-        //         expect($item->sellIn)->toBe(-1);
-        //     });
+                expect($item->quality)->toBe(0);
+                expect($item->sellIn)->toBe(-1);
+            });
 
-        //     it ('actualiza Producto de Café después de la fecha de venta', function () {
-        //         $item = GildedRose::of('Café Altocusco', 10, -10);
+            it ('actualiza Producto de Café después de la fecha de venta', function () {
+                $item = VillaPeruana::of('Café Altocusco', 10, -10);
 
-        //         $item->tick();
+                $item->tick();
 
-        //         expect($item->quality)->toBe(6);
-        //         expect($item->sellIn)->toBe(-11);
-        //     });
+                expect($item->quality)->toBe(6);
+                expect($item->sellIn)->toBe(-11);
+            });
 
-        //     it ('actualiza Producto de Café después de la fecha de venta con calidad 0', function () {
-        //         $item = GildedRose::of('Café Altocusco', 0, -10);
+            it ('actualiza Producto de Café después de la fecha de venta con calidad 0', function () {
+                $item = VillaPeruana::of('Café Altocusco', 0, -10);
 
-        //         $item->tick();
+                $item->tick();
 
-        //         expect($item->quality)->toBe(0);
-        //         expect($item->sellIn)->toBe(-11);
-        //     });
+                expect($item->quality)->toBe(0);
+                expect($item->sellIn)->toBe(-11);
+            });
 
-        // });
+        });
 
     });
 
--- src/VillaPeruana.php
+++ src/VillaPeruana.php
@@ -23,51 +23,98 @@ class VillaPeruana
 
     public function tick()
     {
-        if ($this->name != 'Pisco Peruano' and $this->name != 'Ticket VIP al concierto de Pick Floid') {
-            if ($this->quality > 0) {
-                if ($this->name != 'Tumi de Oro Moche') {
-                    $this->quality = $this->quality - 1;
-                }
-            }
-        } else {
-            if ($this->quality < 50) {
-                $this->quality = $this->quality + 1;
+        
+        if($this->isEditableForProduct()){
+            if($this->isEditableForMinMaxQuality()){
+                $nQualitySubOrAdd = $this->subtractForDateSell();
+                $subQuality = $this->subtractOrAddForProduct();
+                $multSubQuality = $this->isProductCafe();
 
-                if ($this->name == 'Ticket VIP al concierto de Pick Floid') {
-                    if ($this->sellIn < 11) {
-                        if ($this->quality < 50) {
-                            $this->quality = $this->quality + 1;
-                        }
-                    }
-                    if ($this->sellIn < 6) {
-                        if ($this->quality < 50) {
-                            $this->quality = $this->quality + 1;
-                        }
-                    }
-                }
+                list($nQualitySubOrAdd, $subQuality) = $this->isProductTicket($nQualitySubOrAdd, $subQuality);
+
+                $this->subtractOrAddQuality($subQuality, $nQualitySubOrAdd, $multSubQuality);
+
+                $this->checkMinMaxQuality();
+            }
+            
+            $this->sellIn -= 1;
+            
+        }
+        
+    }
+
+    function isEditableForProduct(){
+        // it is Editable for Product
+        return $this->name != 'Tumi de Oro Moche';
+    }
+
+    function isEditableForMinMaxQuality(){
+        // it is Editable for Min or Max Quality
+        return $this->quality > 0 && $this->quality < 50;
+    }
+
+    function subtractForDateSell(){
+        // it double subtraction if date has passed
+        return ($this->sellIn > 0)? 1 : 2;
+    }
+
+    function subtractOrAddForProduct(){
+        // it subtraction or add for Product
+        $subQuality = true;
+        if($this->name == 'Pisco Peruano' || $this->name == 'Ticket VIP al concierto de Pick Floid'){
+            $subQuality = false;
+        }
+        return $subQuality;
+    }
+
+
+    function isProductTicket($nQualitySubOrAdd, $subQuality){
+        // it Product Ticket Add Quality less days sell, but sell is passed quality subtract to cero
+        if($this->name == 'Ticket VIP al concierto de Pick Floid'){
+                    
+            if($this->sellIn <= 10){
+                $nQualitySubOrAdd = 2;
+            }
+
+            if($this->sellIn <= 5){
+                $nQualitySubOrAdd = 3;
+            }
+
+            if($this->sellIn < 1){
+                $nQualitySubOrAdd = $this->quality;
+                $subQuality = true;
             }
         }
 
-        if ($this->name != 'Tumi de Oro Moche') {
-            $this->sellIn = $this->sellIn - 1;
-        }
+        return [$nQualitySubOrAdd, $subQuality];
+    }
 
-        if ($this->sellIn < 0) {
-            if ($this->name != 'Pisco Peruano') {
-                if ($this->name != 'Ticket VIP al concierto de Pick Floid') {
-                    if ($this->quality > 0) {
-                        if ($this->name != 'Tumi de Oro Moche') {
-                            $this->quality = $this->quality - 1;
-                        }
-                    }
-                } else {
-                    $this->quality = $this->quality - $this->quality;
-                }
-            } else {
-                if ($this->quality < 50) {
-                    $this->quality = $this->quality + 1;
-                }
-            }
+
+    function isProductCafe(){
+        // it double subtraction if date has passed
+        $multSubQuality = 1;
+        if($this->name == 'Café Altocusco'){
+            $multSubQuality = 2;
+        }
+        return $multSubQuality;
+    }
+
+
+    function checkMinMaxQuality(){
+        // it check Min or Max Quality
+        $this->quality = ($this->quality < 0)? 0 : $this->quality;
+        $this->quality = ($this->quality > 50)? 50 : $this->quality;
+    }
+
+
+    function subtractOrAddQuality($subQuality, $nQualitySubOrAdd, $multSubQuality){
+        // it subtract or add quality
+        if($subQuality){
+            $this->quality =  $this->quality - ($nQualitySubOrAdd * $multSubQuality);
+        }else {
+            $this->quality =  $this->quality + $nQualitySubOrAdd;
         }
     }
+
+
 }
